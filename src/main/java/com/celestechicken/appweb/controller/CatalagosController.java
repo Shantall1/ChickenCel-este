30d2b76a7eeb44e285d50fa8b1e4e70727572fd1
JhinmerHectorAlvarez <jhinmerhectoralvarez@gmail.com>
2021-05-04 12:55:26 -0500

    avanzando conatacto usuario  controller

---
+++ src/main/java/com/celestechicken/appweb/controller/CatalagosController.java
@@ -0,0 +1,17 @@
+package com.celestechicken.appweb.model;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+@Controller
+public class CatalagosController {
+    private static final String Index="catalogo/index";
+@getMapping("/catalogo/index")
+public String index(Model MODEL);{
+return INDEX;
+}
+}
