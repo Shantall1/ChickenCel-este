4f7bd991bd7a7a605d490fe7c50edf1b41e484b2
IvethA <iveth_avq@hotmail.com>
2021-05-05 22:26:00 -0500

    avances

--- src/main/java/com/celestechicken/appweb/repository/ClienteRepository.java
+++ src/main/java/com/celestechicken/appweb/repository/ClienteRepository.java
@@ -1,11 +1,12 @@
 package com.celestechicken.appweb.repository;
+
 import com.celestechicken.appweb.model.Cliente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface  ClienteRepository extends JpaRepository<Cliente, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
     
 }
